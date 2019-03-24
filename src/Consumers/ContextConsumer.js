98a0f7f51b749786ff28b9524c1d9d87930a99ac
Tomasz Fiechowski <tomasz.fiechowski@codility.com>
2019-03-24 20:48:17 +0100

    Add and use CombinedConsumer

--- src/Consumers/ContextConsumer.js
+++ src/Consumers/ContextConsumer.js
@@ -1,13 +1,13 @@
 import React from "react";
 import PhotosList from "../components/PhotoList";
-import PhotosContext from "../contexts/PhotosContext";
+import { ContextConsumer } from "../contexts";
 
 export default function PhotoListContextConsumer() {
   return (
-    <PhotosContext.Consumer>
-      {({ photos, fetchPhotos }) => (
+    <ContextConsumer>
+      {({ photos: { photos, fetchPhotos } }) => (
         <PhotosList photos={photos} fetchPhotos={fetchPhotos} />
       )}
-    </PhotosContext.Consumer>
+    </ContextConsumer>
   );
 }
--- src/contexts/index.js
+++ src/contexts/index.js
@@ -1,2 +1,18 @@
-export { default as PhotosContext } from "./PhotosContext";
-export { default as ThemeContext } from "./ThemeContext";
+import React from "react";
+import PhotosContext from "./PhotosContext";
+import ThemeContext from "./ThemeContext";
+
+function ContextConsumer({ children }) {
+  return (
+    <PhotosContext.Consumer>
+      {photos => (
+        <ThemeContext.Consumer>
+          {theme => children({ photos, theme })}
+        </ThemeContext.Consumer>
+      )}
+    </PhotosContext.Consumer>
+  );
+}
+
+export { ContextConsumer };
+export { PhotosContext, ThemeContext };
