3fb30fd52dcf66fde996ca20e163cc154b8f555b
Tomasz Fiechowski <tomasz.fiechowski@codility.com>
2019-03-24 19:41:06 +0100

    Setup Context and consumers

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Row from "./components/Row";
+import PhotoListContextConsumer from "./Consumers/ContextConsumer";
+import PhotoListContextType from "./Consumers/contextType";
+import PhotoListUseContext from "./Consumers/useContext";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>Context demo</h1>
+
+          <Row>
+            <div>
+              <h3>Context Consumer</h3>
+              <PhotoListContextConsumer />
+            </div>
+            <div>
+              <h3>ContextType</h3>
+              <PhotoListContextType />
+            </div>
+            <div>
+              <h3>useContext</h3>
+              <PhotoListUseContext />
+            </div>
+          </Row>
         </header>
       </div>
     );
