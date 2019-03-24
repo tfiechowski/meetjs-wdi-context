693a34a7c24782b6399ea259151184f531b140f0
Tomasz Fiechowski <tomasz.fiechowski@codility.com>
2019-03-24 20:58:15 +0100

    Update PhotoList component to respect theme

--- src/components/PhotoList.js
+++ src/components/PhotoList.js
@@ -1,10 +1,14 @@
 import React from "react";
 
-export default function PhotosList({ photos, fetchPhotos }) {
+export default function PhotosList({ photos, fetchPhotos, theme }) {
+  const className = `photo-${theme}`;
+
   return (
     <div>
       {photos.map(photo => (
-        <div key={photo.id}>{photo.title}</div>
+        <div key={photo.id} className={className}>
+          {photo.title}
+        </div>
       ))}
 
       <button onClick={fetchPhotos}>Fetch more photos</button>
